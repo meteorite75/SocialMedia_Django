551d9339394e5a5838fade45a9c9c100444e3877
shahab <razavi.shahab@yahoo.com>
2022-12-05 13:54:32 +0330

    first name and last name field added to sign up page and "profile modle"

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -6,6 +6,8 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
+    first_name = models.CharField(max_length=100 , default='first_name')
+    last_name = models.CharField(max_length=100, default='last_name')
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images' , default='default_pic.png')
     location = models.CharField(max_length= 100, blank= True)
