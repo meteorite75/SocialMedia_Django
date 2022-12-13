55614ec8799885b7542a12de5ba571849e677ec1
shahab <razavi.shahab@yahoo.com>
2022-12-13 12:52:10 +0330

    model "post" created and registerd to admin pannel

--- mysite/posts/models.py
+++ mysite/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
+from datetime import datetime
 
-# Create your models here.
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True , default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_image')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    num_of_likes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.caption
