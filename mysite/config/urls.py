24286db85c3e29ddeb5362f488fc529d7e33f619
shahab <razavi.shahab@yahoo.com>
2022-12-13 11:54:07 +0330

    "posts" app created

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('posts/', include('posts.urls')),
 ]
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,
