4348da2a31b0ab99fdb88907877da2a1fa496bb0
shahab <razavi.shahab@yahoo.com>
2022-12-14 11:43:32 +0330

    profile picture is now displayed on the home page

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -8,7 +8,9 @@ from .models import Profile
 
 @login_required(login_url='login')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username = request.user.username)
+    user_profile = Profile.objects.get(user = user_object)
+    return render(request, 'index.html', {'user_profile': user_profile})
 
 @login_required(login_url='login')
 def settings(request):
