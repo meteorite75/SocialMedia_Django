4c1894fa7aca7bdde5444427c7d0f791766aa57d
shahab <razavi.shahab@yahoo.com>
2022-12-15 12:21:12 +0330

    "post_upload" feature added

--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Post
 
 @login_required(login_url='login')
 def upload(request):
-    return redirect('/')
+    if request.method == 'POST':
+        user = request.user.username
+        post_image = request.FILES.get('post_image')
+        caption = request.POST['caption']
+        
+        new_post = Post.objects.create(user=user, image = post_image, caption = caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return redirect('/')
