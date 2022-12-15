ad605cf9b9512dbc471748571b579f9d2cc8fced
shahab <razavi.shahab@yahoo.com>
2022-12-15 10:44:17 +0330

    "Post model"and "Profile model"'s test feature added

--- mysite/main/tests.py
+++ mysite/main/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Profile
 
-# Create your tests here.
+class ProfileTest(TestCase):
+    def setUp(self):
+        
+        self.user = get_user_model().objects.create_user(
+            username = 'shahab',
+            password = '1234'
+            )
+        
+        self.profile = Profile.objects.create(
+            user = self.user,
+            id_user = '1',
+            first_name = 'test first_name',
+            last_name = 'test last_name',
+            bio = 'test bio',
+            profileimg = 'test profileimg',
+            location = 'test location',
+        )
+        
+    def test_model(self):
+        self.assertEqual(f'{self.profile.user}', 'shahab')
+        self.assertEqual(f'{self.profile.id_user}', '1')
+        self.assertEqual(f'{self.profile.first_name}', 'test first_name')
+        self.assertEqual(f'{self.profile.last_name}', 'test last_name')
+        self.assertEqual(f'{self.profile.profileimg}', 'test profileimg')
+        self.assertEqual(f'{self.profile.location}', 'test location')
--- mysite/posts/tests.py
+++ mysite/posts/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Post
 
-# Create your tests here.
+class PostTest(TestCase):
+    def setUp(self):
+
+        self.post = Post.objects.create(
+            user = 'test user',
+            image = 'test image',
+            caption = 'test caption',
+            created_at = '1111-11-11 11:11',
+            num_of_likes = '0',
+        )
+        
+    def test_model(self):
+        self.assertEqual(f'{self.post.user}', 'test user')
+        self.assertEqual(f'{self.post.image}', 'test image')
+        self.assertEqual(f'{self.post.caption}', 'test caption')
+        self.assertEqual(f'{self.post.created_at}', '1111-11-11 11:11')
+        self.assertEqual(f'{self.post.num_of_likes}', '0')
