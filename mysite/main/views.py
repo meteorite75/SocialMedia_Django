39bfb5313b592c3cdead01926a9f5e4c6fad2577
shahab <razavi.shahab@yahoo.com>
2023-11-27 09:45:51 +0330

    add my account option and delete some html codes

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -13,7 +13,7 @@ def index(request):
     user_object = User.objects.get(username = request.user.username)
     user_profile = Profile.objects.get(user = user_object)
     
-    user_following_list = []
+    user_following_list = [] 
     feed = []
     
     user_following = FollowersCount.objects.filter(follower = request.user.username)
